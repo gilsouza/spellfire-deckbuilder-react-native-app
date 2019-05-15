734188b561f77c14d090320ced11f58f9d0c7c42
Gil Souza <gilmarcs@gmail.com>
2019-05-15 11:31:32 -0300

    Configuração e criação de template para projeto

---
+++ clear-cache.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+
+#watchman watch-del-all && 
+#rm -rf $TMPDIR/react-native-packager-cache-* &&
+#rm -rf $TMPDIR/metro-bundler-cache-* && 
+rm -rf node_modules/ &&
+npm cache clean --force &&
+npm install && 
+npm start -- --reset-cache
