6613c02fbbdc532c590c05c78f15f5fc03e83067
Gil Souza <gilmarcs@gmail.com>
2019-05-16 12:31:41 -0300

    Inicio da inclusão de redux

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,9 @@ module.exports = {
     rules: {
         // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
         // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+        '@typescript-eslint/explicit-member-accessibility': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off'
+
     },
     settings: {
         react: {
